28297e2f69f3dd2b37d99a7c7ca59eed3fbd942b
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-12-23 20:01:08 +0100

    Creación de Alias y uso de nuevos Flujos de control

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A' | 'B' | 'F';
 
 @Component({
   standalone: true,
@@ -7,5 +9,31 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
+  public frameworks = signal(['Angular','Vue','Svelte','Qwik','React']);
+  public frameworks2 = signal([]);
+
+  public toggleContent() {
+    this.showContent.update(value => !value )
+  }
+
+  public toggleGrade() {
+    switch (this.grade())
+    {
+      case ('A'):
+        this.grade.set('B')
+        return;
+      case ('B'):
+        this.grade.set('F')
+        return;
+      case ('F'):
+        this.grade.set('A')
+        return;
+    }
+
+  }
 
 }
