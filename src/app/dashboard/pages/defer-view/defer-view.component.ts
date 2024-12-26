20e423ade53b6efe4218b0a83e1ae4489e6e14df
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-12-26 17:06:57 +0100

    Uso de @defer y diferentes opciones del mismo

--- src/app/dashboard/pages/defer-view/defer-view.component.ts
+++ src/app/dashboard/pages/defer-view/defer-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './defer-view.component.html',
-  styles: ``
+  imports: [TitleComponent, HeavyLoadersSlowComponent],
+  templateUrl: './defer-view.component.html'
 })
 export default class DeferViewComponent {
 
