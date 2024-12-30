4911b9c29f1f7ef2564df05c048e70aeece96c64
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-12-30 17:50:22 +0100

    Instalación de Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,7 +16,7 @@ export const appConfig: ApplicationConfig = {
         skipInitialTransition: true
       }),
     ),
-
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync()
   ]
 };
