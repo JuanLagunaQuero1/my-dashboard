3a2d4c564498542d55886e0a8c48b5cde5a2a592
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-12-30 16:04:24 +0100

    Uso de nuevos Inputs y Outputs con Señales

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, signal } from '@angular/core';
+import { interval, take, tap } from 'rxjs';
+
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { Product } from '@interfaces/product.interface';
 
 @Component({
   selector: 'app-input-output',
@@ -8,4 +11,45 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
   templateUrl: './input-output.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class InputOutputComponent { }
+export default class InputOutputComponent implements OnDestroy {
+
+  public products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Product 1',
+      quantity: 0,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      quantity: 0,
+    },
+  ]);
+
+  private intervalSubscription = interval(1000)
+    .pipe(
+      tap(() => {
+        this.products.update((products) => [
+          ...products,
+          {
+            id: products.length + 1,
+            name: `Product ${products.length + 1}`,
+            quantity: 0,
+          },
+        ]);
+      }),
+      take(7)
+    )
+    .subscribe();
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+  }
+
+  public updateProduct(product: Product, quantity: number): void {
+    this.products.update((products) =>
+      products.map((p) => (p.id === product.id ? {...p, quantity} : p))
+    );
+  }
+
+}
